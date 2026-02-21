b056034551aaef673fac85ef8cb3f9d1df84d79c
Ruben Agustin <r.agussglz@gmail.com>
2026-02-21 12:16:43 +0100

    partition manager and polkit agent

--- common/base.nix
+++ common/base.nix
@@ -136,6 +136,7 @@
     shared-mime-info
     desktop-file-utils
     libsForQt5.qtstyleplugins
+    kdePackages.partitionmanager
     # gnome-menus # just to find packages, 500KB approx
 
     udisks
@@ -253,6 +254,22 @@
   services.displayManager.dms-greeter = {
     enable = true;
     compositor.name = "niri";
+  };
+
+  security.polkit.enable = true;
+
+  systemd.user.services.polkit-kde-authentication-agent-1 = {
+    description = "polkit-kde-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
   };
 
   # Rebuild sycoca db 
